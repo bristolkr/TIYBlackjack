ba8f5892c754d3be162535e609320de4903223ab
Kelly Bristol <bristolkr@gmail.com>
2015-05-08 11:25:42 -0400

    cards

---
+++ cards.rb
@@ -0,0 +1,10 @@
+# #card.rb (Card class) (use arrays)
+#     - use to designate each of the 52 cards 
+#     - must use suits (13 cards per suit)
+#     - must use face cards 
+
+card_values = [1..10]
+face_card_value = 10
+ace_value1 = 1
+ace_value2 = 11
+suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
