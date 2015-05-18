7e3af716f6507a4e2a777229a0d58a491318ee82
Kelly Bristol <bristolkr@gmail.com>
2015-05-18 17:20:24 -0400

    SO CLOSE

--- blackjack.rb
+++ blackjack.rb
@@ -59,10 +59,8 @@ class Greeting
 .....................=~=++++++++++++++++++++++++=,?.............................
 ........................?=~:~~==+++++++++++===::~+..............................
 ...........................?+=~:::~~~~~~~~:~~=+?................................
-
 "
     puts "
-
 @@@@@@      @@        @@@@@@@@@   @@@@@@@@@   @@      @@         @@   @@@@@@@@@   @@@@@@@@@@   @@     @@
 @@@@@@@@    @@        @@     @@   @@@@@@@@@   @@     @@          @@   @@     @@   @@@@@@@@@@   @@    @@
 @@     @@   @@        @@     @@   @@          @@    @@           @@   @@     @@   @@           @@   @@
@@ -73,9 +71,6 @@ class Greeting
 @@     @@   @@        @@     @@   @@          @@    @@           @@   @@     @@   @@           @@    @@
 @@@@@@@     @@@@@@@   @@     @@   @@@@@@@@@   @@     @@   @@     @@   @@     @@   @@@@@@@@@@   @@     @@
 @@@@@       @@@@@@@   @@     @@   @@@@@@@@@   @@      @@   @@@@@@@@   @@     @@   @@@@@@@@@@   @@      @@
-
-
-
 "
     puts "I’ll build by own theme park. With black jack, and hookers. In fact, forget the park! Welcome to Blackjack! What's your name? "
     @user = gets.chomp.capitalize
@@ -85,27 +80,22 @@ class Greeting
 
     if rules == "Y"
       puts "
-
 Blackjack, also called 21, is a card game of probability. 
 The goal is to reach a face-value total close or equal to 21 without going over (bust).
     
 Blackjack is an Ace plus a Face (K, Q, J) or 10 card.
-
 Aces are high -- they have a value of 11.
-
 Both you and the Dealer will receive an initial deal of two cards.
     
 If the Dealer receives Blackjack, you lose.
 If you receive Blackjack, you win.
 If you both receive Blackjack, the result is a draw and the game ends in a draw.
-
 If nobody receives Blackjack:
 You must decide to keep your current cards (stay), or draw another (hit) until you reach 21 or bust.
 After your cards have been dealt, it is the Dealer's turn.
 The Dealer MUST hit on 15 and stay on 16.
 The player with the highest face-value total without going bust wins.
 If the Dealer and player tie, the result is a draw.
-
 Let's play, #{@user}!
 "
     elsif rules == "N"
@@ -115,59 +105,16 @@ Let's play, #{@user}!
   end
 end
 
-# class Hand
+class Hand
  
-#   attr_accessor :player_hand, :dealer_hand, :player_points, :dealer_points
-
-#   def initialize
-#     @player_hand = []
-#     @dealer_hand = []
-#     @player_points = nil
-#     @dealer_points = nil
-#     @deck_of_cards = Deck.new
-#   end
-
-#   def deal_initial
-#     2.times do
-#       @player_hand.push(@deck_of_cards.draw)
-#       @dealer_hand.push(@deck_of_cards.draw)
-#     end
-#   end
-
-#   def player_hand_points
-#     @player_points = 0
-#     @player_hand.each do |card|
-#       @player_points += card.value
-#     end
-#     @player_points
-#   end
-
-#   def dealer_hand_points
-#     @dealer_points = 0
-#     @dealer_hand.each do |card|
-#       @dealer_points += card.value
-#     end
-#     @dealer_points
-#   end
-# end
-
-greeting = Greeting.new
-
-
-class Game 
- 
-attr_accessor :player_hand, :dealer_hand, :player_points, :dealer_points, :winner, :loser, :draw
+  attr_accessor :player_hand, :dealer_hand, :player_points, :dealer_points, :deck_of_cards
 
   def initialize
     @player_hand = []
     @dealer_hand = []
     @player_points = nil
     @dealer_points = nil
-    @winner = nil
-    @loser = nil
-    @draw = nil
     @deck_of_cards = Deck.new
-
   end
 
   def deal_initial
@@ -187,76 +134,110 @@ attr_accessor :player_hand, :dealer_hand, :player_points, :dealer_points, :winne
 
   def dealer_hand_points
     @dealer_points = 0
-    @dealer_hand.each do |card| 
+    @dealer_hand.each do |card|
       @dealer_points += card.value
     end
-    @dealer_points  
+    @dealer_points
+  end
+end
+
+greeting = Greeting.new
+
+hand = Hand.new
+
+hand.deal_initial
+
+puts "#{greeting.user}, your cards are:"
+  hand.player_hand.each { |card| puts card.to_s }
+
+puts "
+My face-up card is: " + hand.dealer_hand.first.to_s
+
+puts "
+Your total is: #{hand.player_hand_points.to_s}"
+
+
+class Game 
+
+  attr_accessor :player_points, :dealer_points, :winner, :loser, :draw, :hand
+
+  def initialize(gameHandGlobal)
+    
+    winner = nil
+    loser = nil
+    draw = nil
+
+    @hand = gameHandGlobal
+
   end
 
-  puts "Your cards are:"
-    player_hand.each { |card| puts card.to_s }
+  def deal_player
+    @hand.player_hand.push(@hand.deck_of_cards.draw)
+  end
 
-    puts "
-    My face-up card is: " + dealer_hand.first.to_s
-
-    puts "
-    Your total is: #{@player_points.to_s}"
-  
+  def deal_dealer
+    @hand.dealer_hand.push(@hand.deck_of_cards.draw)
+  end
 
   def play
-
-    if @player_points == 21
-      puts "I have a busted ass here, and no one is kissing it. You win!"
-    end
+      if @hand.player_points == 21
+        puts "I have a busted ass here, and no one is kissing it. You win!"
+        exit
+      end
         
-      while @player_points <= 20 do
-        puts "Would you like to stay (S) or hit (H)? "
-        stay_hit = gets.chomp.upcase
-          if stay_hit == "H"
+      loop do 
+       # if @hand.player_points < 21
+         puts "Would you like to stay (S) or hit (H)? "
+         stay_hit = gets.chomp.upcase
+        if stay_hit == "H"
+          while @hand.player_points <= 21 do
             self.deal_player
             puts "Your cards are:"
-            @player_hand.each { |card| puts card.to_s }
-            puts "Your total is: #{@player_hand_points.to_s}"
-              if @player_points >= 22
-                puts "You know what cheers me up? Other people’s misfortune. You bust."
-              end      
-          elsif stay_hit == "S"
-            while @dealer_points < 16 do
-              self.deal_dealer
-              if @dealer_points >= 16 and @dealer_points <= 21
-                @dealer_hand.each { |card| puts card.to_s }
-                @winner = @player_points > @dealer_points
-                @loser = @player_points < @dealer_points
-                @draw = @player_points == @dealer_points
-              end
-            end
+            @hand.player_hand.each { |card| puts card.to_s }
+            puts "Your total is: #{hand.player_hand_points.to_s}"
+            puts "Would you like to stay (S) or hit (H)? "
+            stay_hit = gets.chomp.upcase
           end
-      end
 
-    def deal_player
-      @player_hand.push(@deck_of_cards.draw)
+          if @hand.player_points > 21
+          puts "You know what cheers me up? Other people’s misfortune. You bust."
+            exit
+          end
+
+        elsif stay_hit == "S"
+          while @hand.dealer_points < 16 do
+            self.deal_dealer
+          end
+
+          if @hand.dealer_points >= 16 and @hand.dealer_points <= 21
+            @hand.dealer_hand.each { |card| puts card.to_s }
+            puts "My total is: #{hand.dealer_hand_points.to_s}"
+            winner = @hand.player_points > @hand.dealer_points
+            loser = @hand.player_points < @hand.dealer_points
+            draw = @hand.player_points == @hand.dealer_points
+          end
+        end
+      # end
     end
 
-    def deal_dealer
-      @dealer_hand.push(@deck_of_cards.draw)
-    end
-    
-    if @winner == true
+    if winner == true
       puts "I have a busted ass here, and no one is kissing it. You win!" 
     end
 
-    if @loser == true
+    if loser == true
       puts "You know what cheers me up? Other people’s misfortune. You lose." 
     end
 
-    if @draw == true
+    if draw == true
       puts "We tied! Man, this is fun on a bun!" 
     end
-  end 
+    
+  end #end play method
+
 end
 
-game = Game.new
 
-game.deal_initial
+game = Game.new hand
 
 game.play
+
